06a3b0a1c474b5e581f9406ccad1e06eb40ab68a
Patrick <patrickodell572@gmail.com>
2018-10-26 00:31:52 +0100

    Started Animated Sprite Class

--- Source/Headers/Animated Sprite.h
+++ Source/Headers/Animated Sprite.h
@@ -2,7 +2,11 @@
 #ifndef ANIMATED SPRITE H
 #define ANIMATED SPRITE H
 
-#include <Sprite.h>
+#include <string>
+#include <map>
+
+#include "Sprite.h"
+#include "Globals.h"
 
 class Graphics;
 
@@ -11,6 +15,34 @@ holds logic for animating sprites.
 */
 
 class AnimatedSprite : public Sprite {
+
+public:
+	AnimatedSprite();
+	AnimatedSprite(Graphics &graphics, const std::string &filepath, int sourceX, int sourceY, int width, int height,
+		float posX, float posY, float timeToUpdate);
+
+	/*void PlayAnimation
+	Plays the animation provided it is not already playing.
+	*/
+	void playAnimation(std::string animation, bool once = false);
+
+	/*void update
+	Updates the animated time (timer)
+	*/
+	void update(int elapsedTime);
+
+	/* void draw
+	Draws the sprite to the screen
+	*/
+	void draw(Graphics &graphics, int x, int y);
+
+	/* void setupAnimations
+	A required function that sets up all animations for a sprite.
+	*/
+	virtual void setupAnimations();
+
+private:
+	std::map<std::string, std::>
 
 };
 
